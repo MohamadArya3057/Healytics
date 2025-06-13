2e55ec7d2223e2c66e805beb8de205d8a933ccbd
Izzatialifia <izzatialifiaysf@gmail.com>
2025-06-14 01:55:13 +0700

    edit gejala

--- NEW PROJEKAN PBO/Model/GejalaModel.cs
+++ NEW PROJEKAN PBO/Model/GejalaModel.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NEW_PROJEKAN_PBO.model
+namespace NEW_PROJEKAN_PBO.Model
 {
-    public class Gejala : BaseModel
+    public class GejalaModel : BaseModel
     {
         public string Nama_Gejala { get; set; }
     }
