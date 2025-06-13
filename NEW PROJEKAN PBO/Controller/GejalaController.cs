2e30357e01a86bae9e9b447966452d85fd3053d3
Izzatialifia <izzatialifiaysf@gmail.com>
2025-06-13 23:28:10 +0700

    gejala

---
+++ NEW PROJEKAN PBO/Controller/GejalaController.cs
@@ -0,0 +1,17 @@
+﻿using NEW_PROJEKAN_PBO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEW_PROJEKAN_PBO.controller
+{
+    public interface IRepository<T>
+    {
+        List<T> GetAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
---
+++ NEW PROJEKAN PBO/Model/Gejala.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEW_PROJEKAN_PBO.model
+{
+    public class Gejala
+    {
+        public int Id_Gejala { get; set; }
+        public string Nama_Gejala { get; set; }
+    }
+}
